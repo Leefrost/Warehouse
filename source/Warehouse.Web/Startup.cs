bd2ffc926c6e6ebc96bac12513395a5ca4540ea5
Sergii Lischuk <sergiy.lischuk@clarity-ag.net>
2019-03-04 12:51:06 +0200

    Added bootstrap layout

--- source/Warehouse.Web/Startup.cs
+++ source/Warehouse.Web/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,13 +11,25 @@ namespace Warehouse.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("VueCorsPolicy", builder =>
+                {
+                    builder
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .WithOrigins("http://localhost:8080");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseDefaultFiles();
-            app.UseStaticFiles(); 
+            app.UseStaticFiles();
+            app.UseCors("VueCorsPolicy");
 
             if (env.IsDevelopment())
             {
